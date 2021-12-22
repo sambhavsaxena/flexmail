36e77d47837f6975e8d6a93c297e6c8edd67494b
thatsameguyokay <90185950+thatsameguyokay@users.noreply.github.com>
2021-12-22 15:51:06 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -85,7 +85,7 @@ const Home = () => {
                 <div className="text-center">Enter your identifier and select your domain before going live.</div>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', marginTop: '60px' }}>
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <div className="div" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                     <Row onChange={changed} className="g-2 mx-2">
                         <Col md>
                             <FloatingLabel label="Identifier">
