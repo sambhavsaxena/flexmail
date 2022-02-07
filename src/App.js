8feebf6ae03d55eaacf2fe665246baa38202f021
Sambhav Saxena <sambhavsaxena02@gmail.com>
2022-02-07 12:26:57 +0530

    added error page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './screens/Home';
 import About from './screens/About';
+import Error from './screens/Error';
 import './index.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
 
 const App = () => {
   return (
@@ -14,6 +14,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/*" element={<Error />} />
       </Routes>
       <Footer />
     </Router>
